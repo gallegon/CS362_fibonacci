e349ed86962d71367c87b082e755637ede3a3aa6
Nicholai Gallegos <gallegon@oregonstate.edu>
2021-03-11 01:58:20 -0800

    added tests for factorial to pytest_fibonacci.py

--- pytest_fibonacci.py
+++ pytest_fibonacci.py
@@ -10,3 +10,11 @@ def test_fib():
 	assert fibonacci.calc_fibonacci(6) == 8
 	assert fibonacci.calc_fibonacci(7) == 13
 
+def test_fact():
+	assert fibonacci.factorial(1) == 1
+	assert fibonacci.factorial(2) == 2
+	assert fibonacci.factorial(3) == 6
+	assert fibonacci.factorial(4) == 24
+	assert fibonacci.factorial(5) == 120
+	assert fibonacci.factorial(6) == 720
+	
